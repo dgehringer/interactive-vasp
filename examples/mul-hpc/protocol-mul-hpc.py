4012499b810f7586005f3f8f810f9ad0734698d2
Dominik Nöger <dominik.noeger@unileoben.ac.at>
2022-01-21 12:00:45 +0100

    :bug: adapt script to work with SLURM

--- examples/mul-hpc/protocol-mul-hpc.py
+++ examples/mul-hpc/protocol-mul-hpc.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Add export PYTHONPATH="/path/to/interactive-vasp"
-# Add export OMP_NUM_THREADS=1
-
+import sys
 import logging
 import numpy as np
-from interactive.runner import execute_coro, run_vasp_calculation
-from pymatgen.io.vasp import Potcar, Poscar, Kpoints
 from pymatgen.core import Structure
+from pymatgen.io.vasp import Potcar, Poscar, Kpoints
+from interactive.runner import execute_coro, run_vasp_calculation
+
+_, executable, *_ = sys.argv
+
+potcar = Potcar.from_file('POTCAR')  # load POTCAR file
+kpoints = Kpoints.gamma_automatic((4, 4, 4))  # generate you custom KPOINTS file here
+
+incar = dict(ENCUT=400, ALGO='Fast')  # put your INCAR here, tags such as INTERACTIVE, ISYM and IBRION will be updated automatically -> you might get a warning
 
 
-potcar = Potcar.from_file('interactive-vasp/POTCAR')
-kpoints = Kpoints.gamma_automatic((4, 4, 4))
-structure = Poscar.from_file('interactive-vasp/POSCAR').structure
-incar = dict(ENCUT=400, ALGO='Fast')
-
-executable_path = '/calc/dnoeger/software/e5-1650/vasp-intel-mpich-mkl/5.4.1/bin/vasp_std'
-
+# will be passed with the stdout_proc keyword -> This function get's executed whenever a line is written to stdout
 def log_stdout(l):
     logging.info(l.decode().rstrip())
 
@@ -30,11 +26,24 @@ def make_positions(i):
 
 positions = (Structure(4.05*np.eye(3), ['Al']*4, np.array(make_positions(i))) for i in range(6))
 
+# next_structure get's called whenever VASP demands a structure. In other words at the beginning and after each ionic step
+# the parameter "p" is the "process_handle" which allows you to access forces and all the history of the calculation so far
+# interactive-vasp will stop once "next_structure" raises "StopIteration"
+# Use this function to generate all the intermediate structures
 def next_structure(p):
-    print("next_structure: ", p.last_ionic_step['forces'] if p.last_ionic_step else "")
     return next(positions)
 
 
 if __name__ == '__main__':
-    # execute_coro(run_programm())
-    execute_coro(run_vasp_calculation(next_structure, incar, kpoints, potcar, executable=f"mpirun -np 4 {executable_path}", directory='calc', stdout=None, stdout_proc=log_stdout))
+    execute_coro(
+        run_vasp_calculation(
+            next_structure, 
+            incar, 
+            kpoints, 
+            potcar, 
+            executable=executable, 
+            directory='calc', 
+            stdout=None, 
+            stdout_proc=log_stdout
+        )
+    )
